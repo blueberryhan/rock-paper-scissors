05259c7ef0b66fd05feca1bbc32d6b1a91771a49
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-02-12 15:57:04 -0800

    create function that compares choices

--- script.js
+++ script.js
@@ -10,4 +10,35 @@ function getComputerChoice() {
 }
 
 
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'you lose, idiot';
+} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'you win, good job!';
+} else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    return 'it\'s a tie';
+} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'you win!';
+} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'you lose :c';
+} else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    return 'it\'s a tie';
+} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return 'you lose, loser';
+} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'you win!!! c:';
+} else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    return 'it\'s a tie';
+}
+
+}
+
+const playerSelection = prompt('please choose either rock, paper, or scissors').toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log('you chose ' + playerSelection);
+console.log('the computer chose ' +computerSelection);
+console.log(playRound(playerSelection, computerSelection));

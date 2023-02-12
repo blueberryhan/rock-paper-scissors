792784a0bf7d8b18163213cae2353a0a4587e231
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-02-12 14:06:51 -0800

    create function that randomly generates com. choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello world!!');
+function getComputerChoice() {
+    let computerChoices = ['rock','paper','scissors'];
+    // declaring the array i.e. the choices that the computer can make //
+    let randomComputerChoice = Math.floor(Math.random() * computerChoices.length);
+    // a random number is generated based on the amount of choices available in the computerChoices array. //
+    // the number generated correlates to its respective string in the array //
+    return computerChoices[randomComputerChoice];
+    // the randomly generated number is then passed //
+    // through the computerChoices array which selects the computer's choice //
+}
+
+
+console.log(getComputerChoice());
